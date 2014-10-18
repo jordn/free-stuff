cdff58d95ca3e66a275deb47744457aff47c5021
Jordan Burgess <jordanburgess@gmail.com>
2014-10-18 17:04:32 +0100

    Styling

--- free-stuff.js
+++ free-stuff.js
@@ -4,21 +4,9 @@ if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
 
-  Template.hello.helpers({
+  Template.upload.helpers({
     counter: function () {
       return Session.get("counter");
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set("counter", Session.get("counter") + 1);
-      MeteorCamera.getPicture({}, function(error, data) {
-        Items.insert({photo: data});
-        console.log(data);
-        // callback(error, data)
-      });
     }
   });
 
@@ -26,6 +14,19 @@ if (Meteor.isClient) {
     console.log('rendering photos');
     return Items.find();
   }
+
+  Template.upload.events({
+    'click button': function () {
+      // increment the counter when button is clicked
+      Session.set("counter", Session.get("counter") + 1);
+      MeteorCamera.getPicture({/*height, width, quality*/}, function(error, data) {
+        if (!error) {
+          Items.insert({photo: data});
+        }
+      });
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
