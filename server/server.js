64d8b25b5d8541d8c77eb9b984f5c81fff8be430
Jordan Burgess <jordanburgess@gmail.com>
2014-10-19 04:07:55 +0100

    Profile images

--- server/server.js
+++ server/server.js
@@ -1,3 +1,11 @@
 Meteor.publish('items', function() {
   return Items.find();
 });
+
+Accounts.onCreateUser(function(options, user) {
+    if (options.profile) {
+        options.profile.picture = "http://graph.facebook.com/" + user.services.facebook.id + "/picture/?width=200&height=200";
+        user.profile = options.profile;
+    }
+    return user;
+});
